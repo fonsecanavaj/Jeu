adc77215223b98804a57999410f22f8daea959d8
fonsecanavaj <143746128+fonsecanavaj@users.noreply.github.com>
2023-09-21 11:33:48 -0400

    Update main.py
    With comments

--- main.py
+++ main.py
@@ -2,16 +2,20 @@
 # 21/09/23
 # TP2 - Jeu de devinettes
 
+
 import random
 
 score = 0
 
 
+# Le code s'arrête
 def exit_game():
     print("\nMerci et au revoir...")
     exit()
     
 
+# Choisir les bornes (input de l'utilisateur) et le nombre (librairie random)
+# Utilisateur entre son essai
 borne_maximale = int(input("Choisissez la borne maximale: "))
 borne_minimale = int(input("Choisissez la borne minimale: "))
 nombre_a_deviner = random.randint(borne_minimale, borne_maximale)
@@ -21,6 +25,9 @@ essai = int(input("\nEntrez votre essai: "))
 
 
 while True:
+
+    # Donner des instructions à l'utilisateur pour gagner en fonction de l'essai
+    # Augmenter le score après chaque essai
 
     if essai > nombre_a_deviner:
         print(f"\nMauvais choix, le nombre est plus petit que {essai}")
@@ -38,6 +45,9 @@ while True:
         print(f"Vous avez réussi en: {score} essai(s)")
         confirmation = input("\nVoulez-vous faire une autre partie (o/n)? ")
 
+        # L'utilisateur décide de réjouer ou quitter la partie
+
+        # Le jeu recommence avec de nouveaux paramètres
         if confirmation == "o":
             borne_maximale = int(input("\nChoisissez la borne maximale: "))
             borne_minimale = int(input("Choisissez la borne minimale: "))
