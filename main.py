50d460a99f3c595485f6d6ed3e320c89e889c871
fonsecanavaj <143746128+fonsecanavaj@users.noreply.github.com>
2023-10-13 12:12:19 -0400

    Update main.py
    pre correction

--- main.py
+++ main.py
@@ -2,30 +2,24 @@
 # 21/09/23
 # TP2 - Jeu de devinettes
 
-
 import random
 
 score = 0
-
-
-# Le code s'arrête
-def exit_game():
-    print("\nMerci et au revoir...")
-    exit()
     
-
 # Choisir les bornes (input de l'utilisateur) et le nombre (librairie random)
 # Utilisateur entre son essai
-borne_maximale = int(input("Choisissez la borne maximale: "))
-borne_minimale = int(input("Choisissez la borne minimale: "))
-nombre_a_deviner = random.randint(borne_minimale, borne_maximale)
-message = f"J’ai choisi un nombre au hasard entre {borne_minimale} et {borne_maximale}. À vous de le deviner..."
-print(message)
-essai = int(input("\nEntrez votre essai: "))
+def status():
+    global nombre_a_deviner, essai
+    borne_maximale = int(input("Choisissez la borne maximale: "))
+    borne_minimale = int(input("Choisissez la borne minimale: "))
+    nombre_a_deviner = random.randint(borne_minimale, borne_maximale)
+    message = f"J’ai choisi un nombre au hasard entre {borne_minimale} et {borne_maximale}. À vous de le deviner..."
+    print(message)
+    essai = int(input("\nEntrez votre essai: "))
 
+status()
 
 while True:
-
     # Donner des instructions à l'utilisateur pour gagner en fonction de l'essai
     # Augmenter le score après chaque essai
 
@@ -49,11 +43,11 @@ while True:
 
         # Le jeu recommence avec de nouveaux paramètres
         if confirmation == "o":
-            borne_maximale = int(input("\nChoisissez la borne maximale: "))
-            borne_minimale = int(input("Choisissez la borne minimale: "))
-            nombre_a_deviner = random.randint(borne_minimale, borne_maximale)
-            essai = int(input("Entrez votre essai: "))
+            status()
             score = 0
 
         else:
-            exit_game()
+            # Le code s'arrête
+            print("\nMerci et au revoir...")
+            exit()
+            
